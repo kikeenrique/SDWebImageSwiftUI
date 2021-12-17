afccbb72b2bc8bd754b407da0b833c23cd96aeba
Enrique Garcia <github@lemambo.com>
2021-12-17 13:31:19 +0100

    + Provide a library dynamic version

--- Package.swift
+++ Package.swift
@@ -13,11 +13,15 @@ let package = Package(
         .library(
             name: "SDWebImageSwiftUI",
             targets: ["SDWebImageSwiftUI"]),
+        .library(
+            name: "SDWebImageSwiftUIDynamic",
+            type: .dynamic,
+            targets: ["SDWebImageSwiftUI"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.10.0")
+        .package(url: "https://github.com/kikeenrique/SDWebImage.git", .branch("master")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -26,6 +30,6 @@ let package = Package(
             name: "SDWebImageSwiftUI",
             dependencies: ["SDWebImage"],
             path: "SDWebImageSwiftUI/Classes"
-        ),
+        )
     ]
 )
